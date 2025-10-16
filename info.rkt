a051ecd1346cf9ded1d8f441379382ae7eea8388
NoahStoryM <NoahStoryM@gmail.com>
2025-10-16 13:07:05 +0800

    `rktm` -> `r0rr`

--- info.rkt
+++ info.rkt
@@ -4,12 +4,12 @@
 (define collection 'multi)
 (define version "0.0")
 
-(define pkg-desc "R6RS Scheme versions of Racket-inspired libraries")
+(define pkg-desc "Scheme versions of Racket-inspired libraries")
 
 (define deps '("base" "r6rs-lib"))
 (define build-deps '("r6rs-doc"))
 #;
-(define scribblings '(("scribblings/rktm.scrbl")))
+(define scribblings '(("scribblings/r0rr.scrbl")))
 
 (define clean '("compiled" "private/compiled"))
 (define test-omit-paths '("test.ss"))
